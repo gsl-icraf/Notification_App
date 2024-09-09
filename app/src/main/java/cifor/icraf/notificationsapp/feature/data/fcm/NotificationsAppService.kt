491de7e2d78c9b40aeb7591a8c5eec76b5825ee8
Emmanuel Muturia™ <science@emmanuelmuturia.com>
2024-09-09 14:39:48 +0300

    Set up FCM Service...

---
+++ app/src/main/java/cifor/icraf/notificationsapp/feature/data/fcm/NotificationsAppService.kt
@@ -0,0 +1,7 @@
+package cifor.icraf.notificationsapp.feature.data.fcm
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class NotificationsAppService : FirebaseMessagingService() {
+
+}
